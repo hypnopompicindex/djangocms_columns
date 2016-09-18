3d984394b41fdb122946e31823216693435f7be3
hypnopompicindex <hypnopompicindex@gmail.com>
2016-09-18 16:44:08 -0400

    added  admin.py & utils.py fro template discover

--- example/djangocms_columns/admin.py
+++ example/djangocms_columns/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
+from inline_ordering.admin import OrderableStackedInline
+import forms
+import models
 
-# Register your models here.
+
+class ColumnInline(OrderableStackedInline):
+
+    model = models.Column
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'src':
+            kwargs.pop('request', None)
+            kwargs['widget'] = forms.AdminColumnWidget
+            return db_field.formfield(**kwargs)
+        return super(ImageInline, self).\
+            formfield_for_dbfield(db_field, **kwargs)
